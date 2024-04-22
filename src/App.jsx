aae935675b15e784deffe7e779b60cb7d2c5f822
otarovvak <134216287+otarovvak@users.noreply.github.com>
2024-04-22 21:37:24 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState("");
+  const calculateResult = (expression) => {
+    try {
+      const result = evaluate(expression);
+      setValue(String(result));
+    } catch (error) {
+      console.error("Error evaluating expression: ", expression);
+    }
+  };
+  return (
+    <div className="container">
+      <div className="calculator">
+        <form>
+          <div className="display">
+            <input type="text" value={value} />
+          </div>
+          <div>
+            <input type="button" value="AC" onClick={(e) => setValue("")} />
+            <input
+              type="button"
+              value="DE"
+              onClick={(e) => setValue(value.slice(0, -1))}
+            />
+            <input
+              type="button"
+              value="."
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="/"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="7"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="8"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="9"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="*"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="4"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="5"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="6"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="+"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="1"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="2"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="3"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="-"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="00"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="0"
+              onClick={(e) => setValue(value + e.target.value)}
+            />
+            <input
+              type="button"
+              value="="
+              className="equal"
+              onClick={(e) => calculateResult(value)}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
